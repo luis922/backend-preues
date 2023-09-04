15a17b653ca40b3f4d1f718c42744642b064247e
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-09-04 18:55:19 -0300

    se agrega token validation a createEssay

--- src/routes/essays.ts
+++ src/routes/essays.ts
@@ -6,7 +6,7 @@ const router = express.Router();
 //aqui se genera un null al correr el servidor
 router.get("/essayQuestions", essay.findEssayQuestions); //obtiene todas las preguntas de un ensayo
 router.get("/allQuestions", essay.findAllEssaysQuestions); //obtiene todas las preguntas de todos los ensayos
-router.post("/newEssay", essay.createEssay);
+router.post("/newEssay", tokenValidation, essay.createEssay);
 router.post("/submitAnswers", tokenValidation, essay.submitAnswers);
 router.get("/submittedEssay", tokenValidation, essay.getSubmittedEssay);
 router.get("/history", tokenValidation, essay.getHistory);

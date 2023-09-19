eebdd717bd942482901546c204fdee14a2ea581f
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-09-19 20:19:28 -0300

    se agregan dos funciones para seleccionar preguntas

--- src/lib/general.ts
+++ src/lib/general.ts
@@ -17,8 +17,41 @@ export function getIdfromToken(token: string) {
   return (<any>payload).id;
 }
 
-/* console.log(
-  getIdfromToken(
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MSwibmFtZSI6InVzZXIxMjM0IiwiaWF0IjoxNjkzNzE3ODk1fQ.FqHGBOsW0WHCfDL3VMutdB_BC0zLSlYahuabXfu_F5A"
-  )
-); */
+export function getRandomQuestionsIndex(
+  numero: number,
+  questionsLenght: number
+) {
+  //obtiene un numero de indices seleccionados de manera aleatoria y sin repetición
+  let randomIndex = Math.floor(Math.random() * questionsLenght); //
+  let indexArray: number[] = [];
+  let index = 0;
+  if (numero > questionsLenght) {
+    console.log("Se esta solictando mas preguntas de las que existen");
+    return indexArray;
+  }
+
+  while (index < numero) {
+    if (!indexArray.includes(randomIndex)) {
+      //si el indice rambom no se encuentra en el array
+      indexArray.push(randomIndex); //se agrega al array
+      index++;
+    }
+    randomIndex = Math.floor(Math.random() * questionsLenght); //selecciona un nuevo indice
+  }
+  return indexArray;
+}
+
+export function getRandomQuestions(numero: number, questions: Array<any>) {
+  //obtiene un numero defindo por la variable "numero" de preguntas seleccionadas de manera aleatoria y sin repetición
+  let indexArray: number[] = getRandomQuestionsIndex(numero, questions.length);
+  let selectedQuestions: any[] = [];
+  for (let index = 0; index < indexArray.length; index++) {
+    selectedQuestions.push(questions[indexArray[index]]);
+  }
+  return selectedQuestions;
+}
+async function testFunction() {
+  console.log(getRandomQuestions(10, [1, 2, 3, 4, 5, 6]));
+}
+
+/* testFunction(); */

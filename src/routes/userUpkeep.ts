2ab502ad0308e3d483f425d9b427e5a230e0a704
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-09-23 16:19:22 -0300

    se agrega enpoint para cambiar contraseña

--- src/routes/userUpkeep.ts
+++ src/routes/userUpkeep.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import * as userUpkeep from "../controllers/userUpkeep.controllers";
+import { tokenValidation } from "../controllers/tokenValidation";
 
 const upkRouter = express.Router();
 
 /* upkRouter.delete("/delubyid", userUpkeep.deleteUserByID);
 upkRouter.delete("/delubyname", userUpkeep.deleteUserByName); */
-upkRouter.post("/recoverPassword", userUpkeep.recoverPassword);
+upkRouter.post("/recoverPassword", userUpkeep.recoverPassword); //Recupera contraseña por correo
+upkRouter.patch("/changePassword", tokenValidation, userUpkeep.changePassword); //Cambia contraseña desde el perfil de ususario
 export default upkRouter;

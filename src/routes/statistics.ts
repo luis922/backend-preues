c07adb7c4b6e17664afb97e5eef5323438f8b25f
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-10-03 19:34:33 -0300

    se agrega endpoint para obtener promedio de puntaje

--- src/routes/statistics.ts
+++ src/routes/statistics.ts
@@ -5,5 +5,5 @@ import { tokenValidation } from "../controllers/tokenValidation";
 const router = express.Router();
 
 router.get("/scores/", tokenValidation, stats.getScores); //obtiene todos los puntajes de un tipo de ensayo no custom
-
+router.get("/averageScore/", tokenValidation, stats.getAverageScores); //obtiene el promedio de puntajes de un tipo de ensayo no custom
 export default router;

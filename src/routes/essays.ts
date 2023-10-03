9b7067f9de06fad544bc63fad580a46c9bf59379
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-10-03 16:15:34 -0300

    se agrega nuevo endpoint

--- src/routes/essays.ts
+++ src/routes/essays.ts
@@ -4,16 +4,18 @@ import { tokenValidation } from "../controllers/tokenValidation";
 
 const router = express.Router();
 
-router.get("/essayQuestions", essay.findEssayQuestions); //obtiene todas las preguntas de un ensayo
-router.get("/allQuestions", essay.findAllEssaysQuestions); //obtiene todas las preguntas de todos los ensayos
-router.post("/newEssay", tokenValidation, essay.createEssay); //Crea nuevos ensayos custom o predefinidos
-router.post("/submitAnswers", tokenValidation, essay.submitAnswers); //Alamcena las respuestas al ensayo
-router.get("/submittedEssay", tokenValidation, essay.getSubmittedEssay); //Obtiene el detalle de un ensayo ya realizado
-router.get("/history", tokenValidation, essay.getHistory); //Obtiene todos los ensayos realizados por el usuario
-router.get("/showCustomEssays", tokenValidation, essay.getCustomEssays); //Listar ensayos custom del usuario
-router.get("/customEssay", tokenValidation, essay.getCustomEssay); //Obtener los datos de un ensayo custom particular
-router.delete("/physicalDelEssay", tokenValidation, essay.physicalDeleteEssay); //Borrado fisico del ensayo
+router.get("/essayQuestions/", essay.findEssayQuestions); //obtiene todas las preguntas de un ensayo
+router.get("/allQuestions/", essay.findAllEssaysQuestions); //obtiene todas las preguntas de todos los ensayos
+router.post("/newEssay/", tokenValidation, essay.createEssay); //Crea nuevos ensayos custom o predefinidos
+router.post("/submitAnswers/", tokenValidation, essay.submitAnswers); //Alamcena las respuestas al ensayo
+router.get("/submittedEssay/", tokenValidation, essay.getSubmittedEssay); //Obtiene el detalle de un ensayo ya realizado
+router.get("/history/", tokenValidation, essay.getHistory); //Obtiene todos los ensayos realizados por el usuario
+router.get("/showCustomEssays/", tokenValidation, essay.getCustomEssays); //Listar ensayos custom del usuario
+router.get("/customEssay/", tokenValidation, essay.getCustomEssay); //Obtener los datos de un ensayo custom particular
+router.delete("/physicalDelEssay/", tokenValidation, essay.physicalDeleteEssay); //Borrado físico del ensayo
+router.delete("/logicalDelEssay/", tokenValidation, essay.logicalDeleteEssay); //Borrado lógico del ensayo
 export default router;
 
 //verificacion que id respuestas correspondan a las preguntas de ensayo custom
 //TRATAR TODOS LOS IDS COMO ENTEROS INCLUSO DESDE EL FRONT
+//actualizar readme con credenciales emailer

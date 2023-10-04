1a4862b8adb591ae8d3ba7eb6e65efe732352d62
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-10-03 21:13:11 -0300

    comentarios

--- src/routes/essays.ts
+++ src/routes/essays.ts
@@ -4,8 +4,8 @@ import { tokenValidation } from "../controllers/tokenValidation";
 
 const router = express.Router();
 
-router.get("/essayQuestions/", essay.findEssayQuestions); //obtiene todas las preguntas de un ensayo
-router.get("/allQuestions/", essay.findAllEssaysQuestions); //obtiene todas las preguntas de todos los ensayos
+router.get("/essayQuestions/", essay.findEssayQuestions); //Obtiene todas las preguntas de un ensayo
+router.get("/allQuestions/", essay.findAllEssaysQuestions); //Obtiene todas las preguntas de todos los ensayos
 router.post("/newEssay/", tokenValidation, essay.createEssay); //Crea nuevos ensayos custom o predefinidos
 router.post("/submitAnswers/", tokenValidation, essay.submitAnswers); //Alamcena las respuestas al ensayo
 router.get("/submittedEssay/", tokenValidation, essay.getSubmittedEssay); //Obtiene el detalle de un ensayo ya realizado

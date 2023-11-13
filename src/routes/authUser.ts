a98d94e5f95140e7a3e8ff9d0ec30e694f4a7d55
Luis Romero <luis.romero.q@mail.pucv.cl>
2023-11-13 14:40:30 -0300

    se elimina archivo sobrante

--- src/routes/authUser.ts
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import * as user from "../controllers/authUser.controllers";
-
-const router = express.Router();
-
-router.post("/signup", user.signup);
-router.post("/login", user.login);
-
-export default router;
